4d808acdf7353fcfadc55e939a3b68ccf564c50a
Evgeny Zakharov <risenforces@gmail.com>
2022-07-06 22:34:59 +0300

    update

--- master.sh
+++ master.sh
@@ -4,6 +4,10 @@ CURRENT_HOSTNAME=$(hostname)
 read -p "Enter hostname [$CURRENT_HOSTNAME]: " HOSTNAME
 export HOSTNAME=${HOSTNAME:-$CURRENT_HOSTNAME}
 
+read -p "Enter load balancer hostname (domain): " LOAD_BALANCER_HOSTNAME
+read -p "Enter bootstrap password for Rancher: " BOOTSTRAP_PASSWORD
+read -p "Enter email for Let's Encrypt notifications: " LETS_ENCRYPT_EMAIL
+
 source <(wget -qO- https://raw.githubusercontent.com/risenforces/rancher-bootstrap/main/prepare.sh)
 
 ##################
@@ -20,3 +24,44 @@ sed -i "s/$CURRENT_HOSTNAME/$HOSTNAME/g" /etc/hosts
 curl -sfL https://get.rke2.io | sh -
 systemctl enable rke2-server.service
 systemctl start rke2-server.service
+
+########################
+## install kubernetes ##
+########################
+
+curl -LO https://dl.k8s.io/release/v1.23.0/bin/linux/amd64/kubectl
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+ln -f /etc/rancher/rke2/rke2.yaml ~/.kube/config
+
+##################
+## install helm ##
+##################
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+rm ./get_helm.sh
+
+###########################
+## install rancher chart ##
+###########################
+
+helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+
+kubectl create namespace cattle-system
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.crds.yaml
+
+helm install cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.7.1
+
+helm install rancher rancher-stable/rancher \
+  --namespace cattle-system \
+  --set hostname=$LOAD_BALANCER_HOSTNAME \
+  --set bootstrapPassword=$BOOTSTRAP_PASSWORD \
+  --set ingress.tls.source=letsEncrypt \
+  --set letsEncrypt.email=$LETS_ENCRYPT_EMAIL \
+  --set letsEncrypt.ingress.class=nginx
